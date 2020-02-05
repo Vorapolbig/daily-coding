21812ed6104442d86655d8c193a8822b52cc7d44
Vorapol Assavasangthong <30745707+Vorapolbig@users.noreply.github.com>
2020-02-05 23:48:31 +0200

    DCC 05022020

--- daily-coding-challenge/04022020.py
+++ daily-coding-challenge/04022020.py
@@ -1,3 +1,7 @@
+"""
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+"""
+
 def solution(n):   
     product_digit = 1
     sum_digit = 0
---
+++ daily-coding-challenge/05022020.py
@@ -0,0 +1,30 @@
+"""
+There are n people whose IDs go from 0 to n - 1 and each person belongs exactly to one group. 
+Given the array groupSizes of length n telling the group size each person belongs to, return the groups there are and the people's IDs each group includes.
+
+You can return any solution in any order and the same applies for IDs. Also, it is guaranteed that there exists at least one solution. 
+"""
+import collections
+
+def solution(groupSize):
+    group = collections.defaultdict(list)
+    group_dict = {}
+
+    for person_id, size in enumerate(groupSize):
+        if size not in group_dict:
+            group_dict[size] = (size,0)
+
+        if len(group[group_dict[size]]) == size:
+            group_dict[size] = (group_dict[size][0], group_dict[size][1]+1)
+
+        group[group_dict[size]].append(person_id)
+        # if size in group_dict.keys():
+        #     group_dict[size].append(person_id)
+        # else:
+        #     group_dict[size] = [person_id]
+
+    return list(group.values())
+    # return list(group_dict.values())
+
+groupSize = [3,3,3,3,3,1,3]
+print(solution(groupSize))
