120cf87aa5f9c8070d9716e74d149c1f1a7e14e1
vorapolbig <vorapolbig@gmail.com>
2020-03-06 18:15:37 +0200

    06022020.py

---
+++ daily-coding-challenge/06032020.py
@@ -0,0 +1,24 @@
+"""
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+"""
+
+def solution(n):
+    output = []
+    for i in range(1,n+1):
+        if i % 15 == 0:
+            output.append("FizzBuzz")
+        elif i % 3 == 0:
+            output.append("Fizz")
+        elif i % 5 == 0:
+            output.append("Buzz")
+        else:
+            output.append(str(i))
+    return output
+
+n = 15
+print(solution(n))
+
+
+
