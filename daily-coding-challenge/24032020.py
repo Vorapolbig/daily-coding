c7132beacfb4fba2d5c896c64a55626364219531
Vorapol <vorapolbig@gmail.com>
2020-03-24 22:28:14 +0200

    DCC 24032020

---
+++ daily-coding-challenge/24032020.py
@@ -0,0 +1,52 @@
+"""
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Example 1:
+
+Given nums = [3,2,2,3], val = 3,
+
+Your function should return length = 2, with the first two elements of nums being 2.
+
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,1,2,2,3,0,4,2], val = 2,
+
+Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
+
+Note that the order of those five elements can be arbitrary.
+
+It doesn't matter what values are set beyond the returned length.
+"""
+
+def solution(nums,val):
+    # return len(nums) - nums.count(val)
+    i = 0
+    c = nums.count(val)
+    while c > 0:
+        if nums[i] == val:
+            nums.pop(i)
+            i -= 1
+            c -= 1
+        i += 1
+    return len(nums)
+
+def solution2(nums,val):
+    len_list = len(nums)
+    final_len = 0
+    for i in range(len_list):
+        if nums[i] != val:
+            if final_len != i:
+                nums[final_len] = nums[i]
+            final_len += 1
+        print(nums)
+    return final_len 
+
+
+nums = [0,1,2,2,3,0,4,2]
+val = 2
+print(solution2(nums,val))
