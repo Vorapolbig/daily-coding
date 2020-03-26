145c6cc39acc94aa5eff7da7c563707f5a2f3c52
Vorapol <vorapolbig@gmail.com>
2020-03-26 23:35:18 +0200

    DCC 26032020

---
+++ daily-coding-challenge/26032020.py
@@ -0,0 +1,16 @@
+"""
+Given a date, return the corresponding day of the week for that date.
+
+The input is given as three integers representing the day, month and year respectively.
+
+Return the answer as one of the following values {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}.
+"""
+
+import datetime
+
+def solution(day, month, year):
+    days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday")
+    
+    return days[datetime.date(year,month,day).weekday()]
+
+print(solution(26,3,2020))
