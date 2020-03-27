8b3b5f5729ff9a1e737d4a9445a13f3045b1f435
Vorapol <vorapolbig@gmail.com>
2020-03-27 23:37:51 +0200

    DCC 27032020

---
+++ daily-coding-challenge/27032020.py
@@ -0,0 +1,32 @@
+"""
+A self-dividing number is a number that is divisible by every digit it contains.
+
+For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+
+Also, a self-dividing number is not allowed to contain the digit zero.
+
+Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
+
+Example 1:
+Input: 
+left = 1, right = 22
+Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+Note:
+
+The boundaries of each input argument are 1 <= left <= right <= 10000.
+"""
+def solution(left,right):
+    output = []
+    for number in range(left,right+1,1):
+        c = 0
+        for i in str(number):
+            if int(i) != 0 and number % int(i) == 0 and c >= 0:
+                c += 1
+            else:
+                c = -1
+            if c == len(str(number)):
+                output.append(number)
+    return output
+
+
+print(solution(1,22))
